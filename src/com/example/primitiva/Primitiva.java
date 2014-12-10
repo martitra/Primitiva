e7fe769b7c686293c6fef270cac2c481e54504d8
martitra <https://github.com/martitra>
2014-12-10 08:48:58 +0100

    Signed-off-by: martitra <https://github.com/martitra>

--- src/com/example/primitiva/Primitiva.java
+++ src/com/example/primitiva/Primitiva.java
@@ -2,9 +2,9 @@ package com.example.primitiva;
 
 public class Primitiva {
 
-	static int tam = 6;
-	static int min = 1;
-	static int max = 49;
+	static int TAM = 6;
+	static int MIN = 1;
+	static int MAX = 49;
 
 	public static int[] generarCombinacion() {
 
@@ -12,21 +12,24 @@ public class Primitiva {
 		// generar numeros ()int
 		// existe nuevos (array)
 
-		int[] numeros = new int[tam];
+		int[] numeros = new int[TAM];
 		int i = 0;
+		int b = 0;
 
 		// cambiar esto para no for, sino para que si se repite no i++ y si no
 		// que se quede hasta que no haya repetidos
-		while (i < tam) { 
-			numeros[i] = generarNumeros();
-			numerosIguales(numeros, i);
-
+		for (i = 0; i < 6; i++) {
+			numeros[i] = 1 + (int) (Math.random() * 49);
+			while (b < i) {
+				numerosIguales(numeros, i);
+			}
+			b = 0;
 		}
 		return numeros;
 	}
 
 	public static int generarNumeros() {
-		int numero = min + (int) (Math.random() * max);
+		int numero = MIN + (int) (Math.random() * MAX);
 		return numero;
 	}
 
